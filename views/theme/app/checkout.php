05a504033f904d6a666b52ed13b629c2f8bf23a5
gustavo <gustavoh.santos735@gmail.com>
2022-02-02 20:20:52 -0300

    atualizacao - meios de pagamento na pagina checkout

--- views/theme/app/checkout.php
+++ views/theme/app/checkout.php
@@ -1,5 +1,9 @@
 <?php $v->layout("theme/app/_base");?>
 
+<?php $v->start("head")?>
+<link rel="stylesheet" href="<?= asset('css/icheck-material.min.css')?>">
+<?php $v->end()?>
+
 <section class="checkout">
     <h1 class="title"><i class="fas fa-clipboard-check"></i> CHECKOUT</h1>
     <!--<div class="frete">
@@ -8,10 +12,11 @@
         <input type="text" id="frete-input" placeholder="Ex: 12345-678">
         <button class="btn">Calcular</button>
     </div>-->
-    <h1>Endereço</h1>
-    <div class="user-address">
+    <form action="<?= $router->route("cart.registerOrder")?>" method="post">
+        <h1>Endereço</h1>
+        <div class="user-address">
             <?php if(!empty($enderecos)): ?>
-                <form action="<?= $router->route("cart.registerOrder")?>" method="post">
+                
                     <select id="address" name="address" required>
                         <option value="">Selecione um endereço</option>
                         <?php foreach($enderecos as $endereco):?>
@@ -30,28 +35,43 @@
                         <b>Rua: </b><span class="street"></span>
                         <span class="house-number"></span>
                     </div>
-                    <br>
-                    <button type="submit" class="btn">CONFIRMAR PEDIDO <i class="fas fa-check"></i></button>
-                </form>
             <?php else:?> 
                 <h1>Você não possui endereços cadastrados</h1>
                 <a href="<?= $router->route("address.register")?>" class="btn"><i class="fas fa-plus"></i> Novo Endereço</a>
             <?php endif;?> 
-    </div>
-    <div class="order-items">
-        <h1>No Seu Carrinho <i class="fas fa-shopping-cart"></i></h1>
-        <?php foreach($produtosCarrinho as $item):?>
-            <div class="item">
-                <div class="img-box">
-                    <img src="<?= asset("/images/products/{$item->image}")?>" alt="">
+        </div>
+
+        <h1>Escolha uma forma de pagamento</h1>
+        <div class="paymentMethods">
+            <?php foreach($metodosPagamento as $metodo):?>
+                <div class="method">
+                    <img src="<?= asset("images/payment/$metodo->imagem")?>" alt="">
+                    <div class="icheck-material-blue">
+                        <input checked type="radio" id="<?= $metodo->nome?>" name="service" value="<?= $metodo->idMetodoPagamento?>"/>
+                        <label for="<?= $metodo->nome?>"><?= $metodo->nome?></label>
+                    </div>
                 </div>
-                <h1 class="product-name"><?= $item->productName?></h1>
-                <span class="amount"><b>Quantidade:</b> <?= $item->amount?></span>
-                <span class="subtotal"><b>Subtotal:</b> <?= 'R$ '.number_format($item->subtotal, 2, ',', '.')?></span>
-            </div>
-        <?php endforeach;?>
-       
-    </div>
+            <?php endforeach?>
+        </div>
+
+        <div class="order-items">
+            <h1>No Seu Carrinho <i class="fas fa-shopping-cart"></i></h1>
+            <?php foreach($produtosCarrinho as $item):?>
+                <div class="item">
+                    <div class="img-box">
+                        <img src="<?= asset("/images/products/{$item->image}")?>" alt="">
+                    </div>
+                    <h1 class="product-name"><?= $item->productName?></h1>
+                    <span class="amount"><b>Quantidade:</b> <?= $item->amount?></span>
+                    <span class="subtotal"><b>Subtotal:</b> <?= 'R$ '.number_format($item->subtotal, 2, ',', '.')?></span>
+                </div>
+            <?php endforeach;?>
+        
+        </div>
+        <button type="submit" class="btn">CONFIRMAR PEDIDO <i class="fas fa-check"></i></button>
+    </form>
+
+   
 </section>
 
 <?php $v->start("scripts")?>
